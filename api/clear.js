b67bea55ea562ca5c42917d28abd7d4809fd8012
VSiJs <145893539+VSiJs@users.noreply.github.com>
2024-07-17 10:38:05 +0700

    Update clear.js

--- api/clear.js
+++ api/clear.js
@@ -1,6 +1,5 @@
 export default async (req, res) => {
     if (req.method === 'POST') {
-        // Giả sử xóa dữ liệu từ cơ sở dữ liệu hoặc dịch vụ lưu trữ
         res.status(200).json({ message: 'Data cleared successfully' });
     } else {
         res.status(405).json({ message: 'Method not allowed' });
