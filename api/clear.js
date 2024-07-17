00ebd5bc53d3ba0f3079aa100e7e38a0f9552fc2
VSiJs <145893539+VSiJs@users.noreply.github.com>
2024-07-17 09:35:35 +0700

    Delete api directory

--- api/clear.js
+++
@@ -1,9 +0,0 @@
-export default async (req, res) => {
-    if (req.method === 'POST') {
-        // Xóa data từ cơ sở dữ liệu hoặc dịch vụ lưu trữ
-
-        res.status(200).json({ message: 'Data cleared successfully' });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
-    }
-};
