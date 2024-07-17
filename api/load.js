70587d6e95f2744dabdb65c546c431b76e88c4ab
VSiJs <145893539+VSiJs@users.noreply.github.com>
2024-07-17 10:37:31 +0700

    Update load.js

--- api/load.js
+++ api/load.js
@@ -1,8 +1,9 @@
 export default async (req, res) => {
     if (req.method === 'GET') {
-        // Tải data từ cơ sở dữ liệu hoặc dịch vụ lưu trữ
-        const data = {}; // Data đã được tải
-
+        const data = {
+            selects: [],
+            textareas: []
+        };
         res.status(200).json(data);
     } else {
         res.status(405).json({ message: 'Method not allowed' });
